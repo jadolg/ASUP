0e0f74a425b195194f6fcbdd63b7e8df98e08935
jadolg <diazorozcoj@gmail.com>
2017-09-14 17:12:58 -0400

    - respond a json with the files you can't download instead of just text - list all files you are downloading - help method

--- ASUP.py
+++ ASUP.py
@@ -14,7 +14,8 @@ def generate_and_download():
         compress_files(DOWNLOAD_PATH)
         return send_from_directory(DOWNLOAD_PATH, 'update.tar.gz', as_attachment=True)
     else:
-        return 'Error downloading ' + ', '.join(download.get('error_files'))
+        return jsonify(
+            {'success': False, 'error_files': download.get('error_files'), 'msg': 'Failed to download files'})
 
 
 @app.route('/add', methods=['POST', ])
@@ -37,6 +38,24 @@ def delete_file():
         return jsonify({'success': True, 'files': files})
     else:
         return jsonify({'success': False, 'files': files, 'msg': 'path param missing or path not in files'})
+
+
+@app.route('/list', methods=['GET', ])
+def list_files():
+    return jsonify({'success': True, 'files': get_files()})
+
+
+@app.route('/help', methods=['GET', ])
+def get_help():
+    help_text = '''
+    <p>Get the file by just opening the home page.<p>
+    <ul>
+        <li>add a new file to download:<code> curl http://localhost:5000/add -d path=apath/over/here/there</code></li>
+        <li>delete a file from download list:<code> curl http://localhost:5000/delete -d path=apath/over/here/there</code></li>
+        <li>list all files: <code>curl http://localhost:5000/list</code></li>
+    </ul>
+    '''
+    return help_text
 
 
 if __name__ == '__main__':
